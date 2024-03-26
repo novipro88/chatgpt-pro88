8c0bd59597f45f6409421a1ded9258b01f2cfb81
novipro88 <noviantipali97@gmail.com>
2024-03-27 06:04:20 +0700

    auth and convex

---
+++ app/about/contant/page.tsx
@@ -0,0 +1,5 @@
+const About = () => {
+  return <div>Contact</div>;
+};
+
+export default About;
---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line
+export default {
+  providers: [
+    {
+      domain: "https://meet-mudfish-24.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ]
+};
